06b209db50a6f1e9a03a8f34bab98bfe1f0b5217
Kris <krgauthi@mtu.edu>
2012-09-30 23:37:56 -0400

    created 3 screens and the basegame implemented the splashscreen

--- Broderoids/src/com/Broders/mygdxgame/BaseGame.java
+++ Broderoids/src/com/Broders/mygdxgame/BaseGame.java
@@ -1,15 +1,12 @@
 package com.Broders.mygdxgame;
 
-
+import com.Broders.Screens.*;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
+
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+
+
 
 
 public class BaseGame extends Game {
@@ -17,46 +14,35 @@ public class BaseGame extends Game {
 	
 	
 	
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	OrthographicCamera Cam;
+	
+	public BaseGame(){
+	
+	
+	}
+
 	
 	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+	public void create() {	
 		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
+		Cam = new OrthographicCamera();
+		Cam.setToOrtho(false, 1024, 576);
+		this.setScreen(new SplashScreen(this));
+		super.render();
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 	}
 
+	
+	public OrthographicCamera getCam(){
+		return Cam;
+	}
+	
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+
 	}
 
-	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-	}
+
 
 	@Override
 	public void resize(int width, int height) {

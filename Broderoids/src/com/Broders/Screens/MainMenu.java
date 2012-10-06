6c0f2f5118b5e51dcd292173dd93f42787778296
Kris <krgauthi@mtu.edu>
2012-10-05 21:04:18 -0400

    added buttons with hit boxes

--- Broderoids/src/com/Broders/Screens/MainMenu.java
+++ Broderoids/src/com/Broders/Screens/MainMenu.java
@@ -15,61 +15,63 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class MainMenu implements Screen{
-	
+
 
 	private SpriteBatch spriteBatch;
-	
-    private Texture Title;
+
+	private Texture Title;
 	private Texture Single;
 	private Texture Multiplayer;
-	
-    
-    private BaseGame myGame;
+	private Texture Settings;
+
+
+	private BaseGame myGame;
 
 	public MainMenu(BaseGame g){
 
 		this.myGame = g;
 		myGame.setMain(this);
-		
-	
+
+
 
 	}
 
-	
-	
+
+
 	@Override
 	public void render(float delta) {
-		
+
 		//handle Input and update Backend
 		//it is up to the backend team to decide if they want to handle input seperatly or not
 		HandleInput();
 		Update();
-		
+
 		//server interactions here?
-		
+
 		//update the models on the screen
 		Paint();
-		
-		
+
+
 
 	}
 
-	
+
 	private void Paint() {
 		//Make a black background
 		GL10 g1 = Gdx.graphics.getGL10();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		g1.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		
+
 		//add buttons and logo to this layer
 		spriteBatch.begin();
-        spriteBatch.draw(Title, 0, -175);
-        spriteBatch.draw(Single, 700, 0,200,200);
-       
-        spriteBatch.end();
-		
-		
+		spriteBatch.draw(Title, 0, -175);
+		spriteBatch.draw(Single, 650, 200,200,200);
+		spriteBatch.draw(Multiplayer, 650, 100,200,200);
+		spriteBatch.draw(Settings, 650, 0,200,200);
+		spriteBatch.end();
+
+
 		//end with updating the cam
 		myGame.getCam().update();
 
@@ -81,8 +83,8 @@ public class MainMenu implements Screen{
 	 * This method is where all of the backend will be calculated
 	 */
 	private void Update() {
-		
-		
+
+
 	}
 
 
@@ -90,25 +92,44 @@ public class MainMenu implements Screen{
 	public void HandleInput(){
 
 		if(Gdx.input.justTouched()){
-			myGame.setScreen(new GameScreen(this.myGame));
+			//make hit boxes
+			if(Gdx.input.getX() >= 650 && Gdx.input.getX() <= 850){
+				
+				//single player game X 650-850 Y 180 - 230
+				if(Gdx.input.getY() >= 180 && Gdx.input.getY() <= 230){
+					myGame.setScreen(new GameScreen(this.myGame,false));
+				}else if(Gdx.input.getY() >= 280 && Gdx.input.getY() <= 330){
+					myGame.setScreen(new GameScreen(this.myGame,true));
+				}else if(Gdx.input.getY() >= 380 && Gdx.input.getY() <= 430){
+					System.out.println("Settings");
+				}
+				
+				
+			}
+			
+			
+			
+			
 		}
-		
+
 		if(Gdx.input.isKeyPressed(Keys.F1)){
 			System.out.println("Mouse Pos: "+Gdx.input.getX()+" "+Gdx.input.getY());
 		}
+
+
 		
 		
-		
-		//backout
+		//backout Fix the quick exit from gamescreen
 		if(Gdx.input.isKeyPressed(Keys.ESCAPE)){
 			Gdx.app.exit();
 		}
+
 	}
 
 
 	@Override
 	public void resize(int width, int height) {
-		
+
 
 	}
 
@@ -119,27 +140,29 @@ public class MainMenu implements Screen{
 	 */
 	@Override
 	public void show() {
-		 Title = new Texture(Gdx.files.internal("data/Broderoids.png"));
-		 Single = new Texture(Gdx.files.internal("data/SinglePlayer.png"));
-			spriteBatch = new SpriteBatch();
+		Title = new Texture(Gdx.files.internal("data/Broderoids.png"));
+		Single = new Texture(Gdx.files.internal("data/SinglePlayer.png"));
+		Multiplayer = new Texture(Gdx.files.internal("data/Multiplayer.png"));
+		Settings = new Texture(Gdx.files.internal("data/Settings.png"));
+		spriteBatch = new SpriteBatch();
 
 	}
 
 	@Override
 	public void hide() {
-		
+
 
 	}
 
 	@Override
 	public void pause() {
-		
+
 
 	}
 
 	@Override
 	public void resume() {
-		
+
 
 	}
 

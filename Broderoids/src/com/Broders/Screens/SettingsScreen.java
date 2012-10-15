2f17337443df048dc5092a78c730639c0c97f88d
Mike <magrimes@mtu.edu>
2012-10-15 14:55:13 -0400

    added skeleton settings class

---
+++ Broderoids/src/com/Broders/Screens/SettingsScreen.java
@@ -0,0 +1,59 @@
+package com.Broders.Screens;
+
+import com.Broders.mygdxgame.BaseGame;
+import com.badlogic.gdx.Screen;
+
+public class SettingsScreen implements Screen {
+
+	BaseGame game;
+	
+	public SettingsScreen(BaseGame g) {
+		this.game = g;
+		myGame.setSettings(this);
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}

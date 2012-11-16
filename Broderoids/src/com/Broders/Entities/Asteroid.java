4a37c290df45e730c0fbab7c4437ce2c161b045a
dementedder <rdbaumei@mtu.edu>
2012-11-16 04:12:16 -0500

    Commented everything in the Entity package. Made a couple of minor tweaks to the code too.

--- Broderoids/src/com/Broders/Entities/Asteroid.java
+++ Broderoids/src/com/Broders/Entities/Asteroid.java
@@ -10,27 +10,59 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
+/**
+ * This is the Asteroid(s) class, which extends Entity. Asteroids are created
+ * by CoreLogic and owned by the server (in multiplayer). Asteroids float in
+ * space and present both a target and a hazard to the ship. There are currently
+ * three different sizes of Asteroid: small, medium, and large. Large asteroids
+ * are spawned initially, but being hit by a Bullet will cause it to be destroyed
+ * and spawn two Asteroids of the next size down (if there is one).
+ * 
+ * @author rdbaumei 
+ * @author ejrinkus 
+ * @author krgauthi
+ *
+ */
 public class Asteroid extends Entity {
 
+	//Enumeration of the Asteroid sizes
 	public static final int LARGE = 0;
 	public static final int MEDIUM = 1;
 	public static final int SMALL = 2;
 
+	//Holds the size of the current Asteroid.
 	private int type;
 
+	/**
+	 * Initializes the superclass Entity with the "asteroid" type, then
+	 * creates a Body Definition object at whatever location is applicable
+	 * and a Fixture Definition object with certain physical properties.
+	 * A new body is created from those definitions.
+	 * 
+	 * The type passed in should be "small", "medium", or "large", depending
+	 * upon the required size.
+	 * 
+	 * @param id
+	 *            Used to uniquely identify this Entity
+	 * @param type
+	 *            The type of this Entity, specifically its size.
+	 * @param c
+	 * 			  The color of the Asteroid.
+	 */
 	public Asteroid(String type, Color c, float x, float y) {
+		//Sets the Entity types.
 		super(type);
-
 		this.setEnt("asteroid");
 
+		//Sets visual properties of the Asteroid.
 		super.setColor(c);
-
-		// if we implement separate files just uncomment the lines above and
-		// comment this line out
 		super.setSprite(Settings.data_path + "broid.png");
 
 		FixtureDef fixDef = new FixtureDef();
 		CircleShape shape = new CircleShape();
+		
+		//These physical properties of the Asteroid are dependent upon
+		//its size.
 		if (type.equals("small")) {
 			this.type = SMALL;
 			super.setSize(3.75f);
@@ -49,155 +81,207 @@ public class Asteroid extends Entity {
 		}
 
 		fixDef.shape = shape;
-		fixDef.restitution = 0.0f;
+		fixDef.restitution = 1f;
 
+		//Setting how the Body interacts with the physics engine
 		BodyDef bodDef = new BodyDef();
 		bodDef.type = BodyType.DynamicBody;
 		bodDef.angularDamping = 0.0f;
 		bodDef.linearDamping = 0.0f;
-
+		bodDef.allowSleep = false;
+		
+		//Setting position, given by the parameters, and direction.
 		bodDef.position.set(x, y);
 		bodDef.angle = (float) (MathUtils.PI * Math.random());
-		bodDef.allowSleep = false;
+
 		super.createBody(bodDef, fixDef);
 
-		// sprite
+		//Getting the value that is used to scale the sprite.
 		float meter = Gdx.graphics.getHeight() / CoreLogic.getHeightScreen();
 
+		//Setting the position of the sprite with respect to the body and
+		//size of the Asteroid.
 		super.getSprite().setOrigin(meter * (this.getSize() / 2),
 				meter * (this.getSize() / 2));
 		super.getSprite().setSize(meter * this.getSize(),
 				meter * this.getSize());
 		super.getSprite().setColor(this.getColor());
 
-		// Set type data
+		//Allowing the Asteroid to be referenced by the Body.
 		super.getBody().setUserData(this);
 	}
 
 	@Override
 	public void Draw(SpriteBatch sb) {
 
+		//Getting the screen dimensions.
 		float screenWidth = Gdx.graphics.getWidth();
 		float screenHeight = Gdx.graphics.getHeight();
 
+		//Getting the center of the Body.
 		float x = super.getBody().getPosition().x - (this.getSize() / 2f);
 		float y = super.getBody().getPosition().y - (this.getSize() / 2f);
 
-		float posX;
-		float posY;
-
-		posX = screenWidth
+		//Setting the absolute screen position to draw the sprite to.
+		float posX = screenWidth
 				* ((x - CoreLogic.getViewPortX()) / CoreLogic.getWidthScreen());
-		posY = screenHeight
+		float posY = screenHeight
 				* ((y - CoreLogic.getViewPortY()) / CoreLogic.getHeightScreen());
 
+		//Draws the sprite on top of the Body.
 		super.getSprite().setPosition(posX, posY);
 		super.getSprite().setRotation((float) super.getAngle());
 		super.getSprite().draw(sb);
 
 	}
-
+	
+	/**
+	 * Overridden from Entity. If any information about the Asteroid needs to change
+	 * with time, the required code will go here. Asteroids are pretty inert, so this
+	 * is empty.
+	 */
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Overridden from Entity. When the Asteroid is it by a Bullet, this method gets
+	 * called. For large and medium Asteroids, it saves the physical information, then
+	 * creates two new Asteroids of the next size smaller where this Asteroid used to be.
+	 * 
+	 */
 	@Override
 	public void destroy() {
+		
+		//If this is a small Asteroid, then skip the rest of this method.
+		if (this.type == SMALL) {
+			return;
+		}
+		
 		Asteroid roid1;
 		Asteroid roid2;
+		
+		//These will have the coordinates for the new Asteroids
 		float x1;
 		float x2;
 		float y1;
 		float y2;
 		float dir;
+		
+		//Setting the physical properties of the new Asteroids, depending
+		//upon the size of this Asteroid.
 		if (this.type == LARGE) {
+			//Setting the positions of the new Asteroids.
 			dir = (float) Math.toRadians(this.getAngle());
 			x1 = (float) (this.getX() + 7.5 * Math.cos(dir));
 			x2 = (float) (this.getX() + 7.5 * Math.cos(dir));
 			y1 = (float) (this.getY() - 7.5 * Math.sin(dir));
 			y2 = (float) (this.getY() - 7.5 * Math.sin(dir));
 
+			//BEGIN DEFINITION: the physical properties of roid1
 			roid1 = new Asteroid("medium", CoreLogic.getGame().gameColor, x1,
 					y1);
 
+			//Setting the initial force applied to roid1 to get it moving.
 			float initForce = (float) (4000 + (2000 * Math.random()));
 			float x = (float) (initForce * Math.cos(dir));
 			float y = (float) (initForce * Math.sin(dir));
 
+			//Applying the initial force to roid1
 			Vector2 f = roid1.getBody().getWorldVector(new Vector2(x, y));
 			Vector2 p = roid1.getBody().getWorldPoint(
 					roid1.getBody().getLocalCenter());
 			roid1.getBody().applyForce(f, p);
 
+			//Giving it some spin
 			float spin = (float) (300 + (250 * Math.random()));
 			if (Math.random() >= 0.5f)
 				spin *= -1;
-
 			roid1.getBody().applyTorque(spin);
+			
+			//Giving the new Body to the physics engine.
 			CoreLogic.getEntityMap().put(roid1.getId(), roid1);
+			//END DEFINITION
 
+			//BEGIN DEFINITION: the physical properties of roid2
 			roid2 = new Asteroid("medium", CoreLogic.getGame().gameColor, x2,
 					y2);
 
+			//Setting the initial force applied to roid2 to get it moving.
 			initForce = (float) (2000 + (1000 * Math.random()));
 			x = (float) (initForce * Math.cos(dir));
 			y = (float) (initForce * Math.sin(dir));
 
+			//Applying the initial force to roid2
 			f = roid2.getBody().getWorldVector(new Vector2(x, y));
 			p = roid2.getBody().getWorldPoint(roid2.getBody().getLocalCenter());
 			roid2.getBody().applyForce(f, p);
 
+			//Giving it some spin
 			spin = (float) (300 + (250 * Math.random()));
 			if (Math.random() >= 0.5f)
 				spin *= -1;
-
 			roid2.getBody().applyTorque(spin);
+			
+			//Giving the new Body to the physics engine.
 			CoreLogic.getEntityMap().put(roid2.getId(), roid2);
+			//END DEFINITION
+			
 		} else if (this.type == MEDIUM) {
+			//Setting the positions of the new Asteroids.
 			dir = (float) Math.toRadians(this.getAngle());
 			x1 = (float) (this.getX() + 3.75 * Math.cos(dir));
 			x2 = (float) (this.getX() + 3.75 * Math.cos(dir));
 			y1 = (float) (this.getY() - 3.75 * Math.sin(dir));
 			y2 = (float) (this.getY() - 3.75 * Math.sin(dir));
 
+			//BEGIN DEFINITION: the physical properties of roid1
 			roid1 = new Asteroid("small", CoreLogic.getGame().gameColor, x1, y1);
 
+			//Setting the initial force applied to roid1 to get it moving.
 			float initForce = (float) (500 + (250 * Math.random()));
 			float x = (float) (initForce * Math.cos(Math.random()*2*Math.PI));
 			float y = (float) (initForce * Math.sin(Math.random()*2*Math.PI));
 
+			//Applying the initial force to roid1
 			Vector2 f = roid1.getBody().getWorldVector(new Vector2(x, y));
 			Vector2 p = roid1.getBody().getWorldPoint(
 					roid1.getBody().getLocalCenter());
 			roid1.getBody().applyForce(f, p);
 
+			//Giving it some spin
 			float spin = (float) (300 + (250 * Math.random()));
 			if (Math.random() >= 0.5f)
 				spin *= -1;
-
 			roid1.getBody().applyTorque(spin);
+			
+			//Giving the new Body to the physics engine.
 			CoreLogic.getEntityMap().put(roid1.getId(), roid1);
+			//END DEFINITION
 
+			//BEGIN DEFINITION: the physical properties of roid2
 			roid2 = new Asteroid("small", CoreLogic.getGame().gameColor, x2, y2);
 
+			//Setting the initial force applied to roid2 to get it moving.
 			initForce = (float) (500 + (250 * Math.random()));
 			x = (float) (initForce * Math.cos(dir));
 			y = (float) (initForce * Math.sin(dir));
 
+			//Applying the initial force to roid1
 			f = roid2.getBody().getWorldVector(new Vector2(x, y));
 			p = roid2.getBody().getWorldPoint(roid2.getBody().getLocalCenter());
 			roid2.getBody().applyForce(f, p);
 
+			//Giving it some spin
 			spin = (float) (300 + (250 * Math.random()));
 			if (Math.random() >= 0.5f)
 				spin *= -1;
-
 			roid2.getBody().applyTorque(spin);
+			
+			//Giving the new Body to the physics engine.
 			CoreLogic.getEntityMap().put(roid2.getId(), roid2);
-		} else {
-
+			//END DEFINITION
 		}
 	}
 }
--- Broderoids/src/com/Broders/Entities/Ship.java
+++ Broderoids/src/com/Broders/Entities/Ship.java
@@ -10,13 +10,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 /**
- * Creates a Ship Entity.
+ * This is the Ship, which is an extension of Entity. The Ship created by the 
+ * client is owned by the client and controlled by the player. Each client 
+ * owns exactly one Ship. The Ship can thrust forward, rotate left and right,
+ * and shoot Bullets.
  * 
  * @author ntpeters
  * @author krgauthi
@@ -25,58 +27,71 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
  */
 public class Ship extends Entity {
 
-	private Boolean thrust;
-	private Boolean shooting;
+	private Boolean thrust; //Is the ship thrusting right now?
+	private Boolean shooting; // Is the ship shooting right now?
 	private Sprite sprite;
 
-	// TODO implement method for sound shooting and death sound
-
 	/**
-	 * Just pass in "classic" Initializes a Ship by creating the appropriate
-	 * physical body and sprite set.
+	 * Initializes the superclass Entity with the "ship" type, then
+	 * creates a Body Definition object at the center of the screen
+	 * and a Fixture Definition object with certain physical properties.
+	 * A new body is created from those definitions.
+	 * 
+	 * For now, the type passed in should be "ship". If we implement multiple
+	 * kinds of ships, this will change.
 	 * 
 	 * @param id
 	 *            Used to uniquely identify this entity
 	 * @param type
 	 *            The type of this entity
 	 * @param playerColor
+	 * 			  The Ship's color, which visually identifies it in multiplayer.
 	 */
 	public Ship(String type, Color playerColor, float x, float y) {
 		super(type);
-		super.setEnt("ship");
+		super.setEnt("ship"); //Temporary, until we add different ship kinds.
+		
+		//Defining the outline vertices of the Body.
 		Vector2 vertices[] = new Vector2[3];
 		vertices[0] = new Vector2(-1.5f, 1.39f);
 		vertices[1] = new Vector2(-1.5f, -1.39f);
 		vertices[2] = new Vector2(2.67f, 0.0f);
 
+		//Making the shape of the Ship be defined by the vertices.
 		PolygonShape shape = new PolygonShape();
 		shape.set(vertices);
 
+		//Defining the physical properties of the Body
 		FixtureDef fixDef = new FixtureDef();
 		fixDef.shape = shape;
 		fixDef.density = 0.75f;
 
+		//Defining how the Body will interact with the physics engine.
 		BodyDef bodDef = new BodyDef();
 		bodDef.type = BodyType.DynamicBody;
 		bodDef.angularDamping = 15.0f;
 		bodDef.linearDamping = 0.2f;
-
+		bodDef.allowSleep = false;
+		
+		//Defining the initial position.
 		bodDef.position.set(x, y);
 		bodDef.angle = MathUtils.PI;
-		bodDef.allowSleep = false;
+
 		super.createBody(bodDef, fixDef);
 
+		//Setting graphical attributes
 		super.setSize(6f);
 		super.setColor(playerColor);
-
 		float meter = Gdx.graphics.getHeight() / CoreLogic.getHeightScreen();
 
+		//Setting the default sprite position with respect to the Body.
 		super.setSprite(Settings.data_path + "ship1.png");
 		super.getSprite().flip(false, true);
 		super.getSprite().setOrigin((meter * this.getSize()) / 2,(meter * this.getSize()) / 2);
 		super.getSprite().setSize(meter * this.getSize(), meter * this.getSize());
 		super.getSprite().setColor(playerColor);
 
+		//Setting the thrusting sprite position with respect to the Body
 		this.thrust = false;
 		Texture tempTexture = new Texture(Gdx.files.internal(Settings.data_path	+ "ship2.png"));
 		this.sprite = new Sprite(tempTexture, 1024, 1024);
@@ -85,12 +100,12 @@ public class Ship extends Entity {
 		this.sprite.setSize(meter * this.getSize(), meter * this.getSize());
 		this.sprite.setColor(this.getColor());
 		
-		//Set type data
+		//Allows the Entity to be reference from the Body.
 		super.getBody().setUserData(this);
 	}
 
 	/**
-	 * Checks if the thrust is engaged/disengaged
+	 * Checks if the ship is currently thrusting.
 	 * 
 	 * @return True if thrusting, false if not
 	 */
@@ -99,7 +114,7 @@ public class Ship extends Entity {
 	}
 
 	/**
-	 * Sets whether the thrust is engaged or disengaged
+	 * Sets whether the thrust is engaged or disengaged.
 	 * 
 	 * @param bool
 	 *            True to enable, false to disable
@@ -109,44 +124,49 @@ public class Ship extends Entity {
 	}
 
 	/**
-	 * Checks if the ship is currently shooting
+	 * Checks if the ship is currently shooting.
 	 * 
-	 * @return true if shooting false if not
+	 * @return true if shooting, false if not
 	 */
 	public Boolean getShooting() {
 		return this.shooting;
 	}
 
 	/**
-	 * Sets if the the ship is shooting or not
+	 * Sets if the the ship is shooting or not.
 	 * 
 	 * @param bool
-	 *            true if shooting false if not shooting
+	 *            true if shooting, false if not shooting
 	 */
 	public void setShooting(boolean bool) {
 		this.shooting = bool;
 	}
 
 	/**
+	 * Overridden from Entity. This method gets the current position and
+	 * rotation of the Body, and draws the sprite on top of it.
+	 * 
 	 * @see Entity#Draw(SpriteBatch)
 	 */
 	@Override
 	public void Draw(SpriteBatch sb) {
 
+		//Getting the screen dimensions
 		float screenWidth = Gdx.graphics.getWidth();
 		float screenHeight = Gdx.graphics.getHeight();
 
+		//Getting the center of the Body.
 		float x = super.getBody().getPosition().x - (this.getSize() / 2f);
 		float y = super.getBody().getPosition().y - (this.getSize() / 2f);
 
-		float posX;
-		float posY;
-
-		posX = screenWidth
+		//Getting the absolute screen location to draw the sprite to.
+		float posX = screenWidth
 				* ((x - CoreLogic.getViewPortX()) / CoreLogic.getWidthScreen());
-		posY = screenHeight
+		float posY = screenHeight
 				* ((y - CoreLogic.getViewPortY()) / CoreLogic.getHeightScreen());
 
+		//Draws the sprite on top of the Body. Which sprite is drawn depends upon
+		//if the ship is thrusting or not.
 		if (this.getThrust()) {
 			this.sprite.setPosition(posX, posY);
 			this.sprite.setRotation((float) super.getAngle());
@@ -159,12 +179,21 @@ public class Ship extends Entity {
 		}
 	}
 
+	/**
+	 * Overridden from Entity. If any information about the Ship needs to change
+	 * with time, the required code will go here. It is currently unneeded by the
+	 * Ship, so it is empty.
+	 */
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Overridden from Entity. If any code needs to execute upon destruction of
+	 * the ship, the required code will go here. Once we implement ship death,
+	 * then things like resetting position and losing a life will be here.
+	 */
 	@Override
 	public void destroy() {
 		

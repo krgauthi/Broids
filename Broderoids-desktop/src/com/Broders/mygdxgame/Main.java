4a994b3a31d924b55b5755cc74d0032a3769aef1
Mike Grimes <magrimes@mtu.edu>
2012-11-26 14:36:37 -0500

    fixed settings error, false == 0

--- Broderoids-desktop/src/com/Broders/mygdxgame/Main.java
+++ Broderoids-desktop/src/com/Broders/mygdxgame/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.resizable = true;
+		cfg.resizable = false;
 		cfg.title = "Broderoids";
 		cfg.useGL20 = false;
 		cfg.width = 1024;	//1280, 720  1024  576 load resolution from file or settings manager?

c2f7930b18b65bca8149bccf624466ee0efa1b24
Mike Grimes <magrimes@mtu.edu>
2012-12-09 20:46:57 -0500

    password entry for private games

--- Broderoids/src/com/Broders/Screens/MultiLobby.java
+++ Broderoids/src/com/Broders/Screens/MultiLobby.java
@@ -3,6 +3,7 @@ package com.Broders.Screens;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 import com.Broders.Logic.Net;
 import com.Broders.Logic.NetException;
@@ -12,6 +13,7 @@ import com.Broders.mygdxgame.BaseGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.GL20;
@@ -37,6 +39,8 @@ public class MultiLobby implements Screen {
 
 	private Sprite whiteSprite;
 	private Sprite arrowSprite;
+	
+	private String gamePassword;
 
 	private ArrayList<String[]> games;
 
@@ -210,12 +214,27 @@ public class MultiLobby implements Screen {
 				// join
 				if (x >= .24 && x <= .44) {
 					String[] name = this.games.get(selectedGame + curPage * 5);
-					Screen temp = Net.joinGame(name[0], name[1]);
+							
+					if (Boolean.parseBoolean(name[1])) {
+						// Enter password					
+						Gdx.input.getTextInput(new TextInputListener() {
+							@Override
+							public void input (String text) {
+								gamePassword = text;
+							}
+							@Override
+							public void canceled () {}
+						}, "Password for game:", "");	
+					}
+					
+					System.out.println("Joining Game with password " + gamePassword);
+					Screen temp = Net.joinGame(name[0], gamePassword);
+						
 					if (temp != null) {
 						myGame.setScreen(temp);
 						// TODO: Dispose of this screen
 					} else {
-						// Trouble - failed to join game
+						System.out.println("Shit broke yo");
 					}
 				} else if (x >= .02 && x <= .22) {
 					myGame.setScreen(BaseGame.screens.get("host"));
